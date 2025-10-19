48bfefcb71cf30419d8bcca0883ba86fb370be47
kiyotaki4 <marlinezxc@gmail.com>
2025-10-19 16:00:15 +0900

    develop added sound to buttons and added animations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // document.querySelector() ищет на странице первый елемент button.
 const start__button = document.querySelector('.start__button'); 
-const work__button = document.querySelector('.work__button'); 
-const rest__button = document.querySelector('.rest__button'); 
+const work__button = document.querySelector('#work__button'); 
+const rest__button = document.querySelector('#rest__button'); 
 const stop__button = document.querySelector('.stop__button');
 const timer = document.querySelector('.pomodoro__time');
 const focus__rest = document.querySelector('.focus__rest');
@@ -12,14 +12,26 @@ let count = 25*60;
 let work = true; 
 let isRunning = false;
 let unique__interval = null;
+const button__sound = document.querySelector("#button__sound")
+const start__sound = document.querySelector("#start__sound")
 //когда нажимаем на кнопку мы запускаем какую то функцию
+function play__sound(){
+    button__sound.currentTime =0;   
+    button__sound.play().catch(()=>{});
+}
+function play__start(){
+    start__sound.currentTime =0;
+    start__sound.play().catch(()=>{});
+}
 start__button.addEventListener('click',()=>{  
+    play__sound();
+   
         if(isRunning){
             return;
         }
         isRunning=true;
         updateTime();
-        
+        play__start();
     unique__interval = setInterval(()=>{
         count--;
         updateTime();
@@ -28,23 +40,33 @@ start__button.addEventListener('click',()=>{
     },1000)
 });
 work__button.addEventListener('click',()=>{
+    play__sound();
+
     work=true;
      count=25*60;
      stopTimer(count);
     updateTime();
     updateMode();
-
+    work__button.classList.add('active');
+    rest__button.classList.remove('active');
+ 
       
 })
 rest__button.addEventListener('click',()=>{
+    play__sound();
+
     work=false;
     count=5*60;
     stopTimer(count);
     updateTime();
     updateMode();
+    rest__button.classList.add('active');
+    work__button.classList.remove('active');
     
 })
 stop__button.addEventListener('click',()=>{
+    play__sound();
+
     stopTimer(25*60);
 })
 function stopTimer(newCount){
